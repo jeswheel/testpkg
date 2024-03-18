0ec556a51c8ed1db46d564a0f976df10ec08e861
Jesse Wheeler <jeswheel@umich.edu>
2024-03-18 12:41:02 -0400

    Added basic package structure

---
+++ R/myfun.R
@@ -0,0 +1,14 @@
+#' myfun
+#'
+#' @param a First argument
+#' @param b Second argument
+#' @param c Third Argument
+#'
+#' @return a + b + c
+#' @export
+#'
+#' @examples
+#' myfun(1, 2, 3)
+myfun <- function(a = 1, b = 2, c) {
+  a + b + c
+}
